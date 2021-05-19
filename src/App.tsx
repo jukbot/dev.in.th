30decd51f91a883469ac9269eb053d85cf89e101
Chukkrit Visitsaktavorn <jukbot@yellotalk.co>
2021-05-20 03:50:06 +0700

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span className="App-icon">🧑🏻‍</span>
+        <span className="App-icon">🧑🏻‍💻</span>
         <h1>Built for Thai Developers!</h1>
-        <h2>Coming soon July 2021 🎉</h2>
+        <h2>Coming soon August 2021 🎉</h2>
         <p>Follow us @devsinth 💌</p>
       </header>
     </div>
