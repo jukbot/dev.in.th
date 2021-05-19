9d1c4fd50b0df79ed7a1e9862f3f87e264d1c630
Chukkrit Visitsaktavorn <jukbot@yellotalk.co>
2021-05-20 03:31:20 +0700

    add symple landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <span className="App-icon">🧑🏻‍</span>
+        <h1>Built for Thai Developers!</h1>
+        <h2>Coming soon July 2021 🎉</h2>
+        <p>Follow us @devsinth 💌</p>
       </header>
     </div>
   )
